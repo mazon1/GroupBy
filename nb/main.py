31708cb6c7dadf1890cd7f66291bd0daf18f53a4
Uchenna Mgbaja <dp_beryl@yahoo.com>
2023-04-26 19:06:45 -0600

    Update main.py

--- nb/main.py
+++ nb/main.py
@@ -224,12 +224,16 @@ def prepare_data_for_plots(uplift_ct, trmnt_test, y_test, X_test_2):
             
 @st.cache_data
 def campaign_results():
+    # Replace with the actual path to the MLflow model in your `nb` folder
+    model_uri = "nb/class_transformation_model"
 
-    # Load the model
-    loaded_model = get_model_uri()
-    
-    # Load data
-    X_test_2, y_test, trmnt_test = load_data_model()
+    # Load the model from the run
+    loaded_model = mlflow.sklearn.load_model(model_uri)
+
+    X_test_2 = pd.read_csv('dat/X_test.csv')
+    y_test = pd.read_csv('dat/y_test.csv')
+    trmnt_test = pd.read_csv('dat/trmnt_test.csv')
+
 
     # Make predictions
     uplift_ct = loaded_model.predict(X_test_2)
